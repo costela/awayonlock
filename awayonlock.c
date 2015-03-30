e0d932885dd61027a1101f38e4458a860649b4e4
ingo <ingo@ingo-schaefer.de>
2015-03-30 15:23:15 +0200

    added support for mate-screensaver

--- awayonlock.c
+++ awayonlock.c
@@ -48,6 +48,7 @@ awayonlock_get_handle(void)
 
 static gboolean plugin_load(PurplePlugin *plugin) {
 	DBusGProxy *dbus_proxy_gnome = NULL;
+        DBusGProxy *dbus_proxy_mate = NULL;
 	DBusGProxy *dbus_proxy = NULL;
 	GError *error = NULL;
 
@@ -103,6 +104,37 @@ static gboolean plugin_load(PurplePlugin *plugin) {
 	 * END Gnome-screensaver specific stuff
 	 */
 
+	/*
+	 * Mate-screensaver specific stuff
+	 */
+	dbus_proxy_mate = dbus_g_proxy_new_for_name( dbus_conn,
+			"org.mate.ScreenSaver",
+			"/org/mate/ScreenSaver",
+			"org.mate.ScreenSaver"
+			);
+
+	if(dbus_proxy_mate == NULL) {
+		purple_debug(PURPLE_DEBUG_ERROR, PACKAGE, N_("failed to get DBus proxy\n"));
+		purple_notify_error(plugin, "Away-on-lock", _("Failed to create a DBus Proxy."), NULL);
+		dbus_g_connection_unref(dbus_conn);
+		return FALSE;
+	}
+
+	dbus_g_proxy_add_signal( dbus_proxy_mate,
+			"ActiveChanged",
+			G_TYPE_BOOLEAN,
+			G_TYPE_INVALID
+			);
+
+	dbus_g_proxy_connect_signal( dbus_proxy_mate,
+			"ActiveChanged",
+			G_CALLBACK(awayonlock_idle_changed_callback),
+			NULL,
+			NULL
+			);
+	/*
+	 * END Mate-screensaver specific stuff
+	 */
 	/*
 	 * freedesktop stuff
 	 */
